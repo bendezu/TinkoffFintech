af1376636e77cd7945bcb4118d6d17a22638a6c7
Bendezu Vladislav <bendezuvlad@gmail.com>
2019-03-02 18:55:11 +0300

    Add basic performance layout

--- app/src/main/java/com/bendezu/tinkofffintech/courses/CoursesFragment.kt
+++ app/src/main/java/com/bendezu/tinkofffintech/courses/CoursesFragment.kt
@@ -12,4 +12,12 @@ class CoursesFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_courses, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fragmentManager?.beginTransaction()
+            ?.replace(R.id.performance_container, PerformanceFragment())
+            ?.replace(R.id.rating_container, RatingFragment())
+            ?.replace(R.id.completed_courses_container, CompletedCoursesFragment())
+            ?.commit()
+    }
 }
