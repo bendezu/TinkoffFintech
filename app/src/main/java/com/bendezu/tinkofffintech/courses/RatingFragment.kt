d6a43f8b70405ec77bebc375b9a6e7dbd6827bbc
Bendezu Vladislav <bendezuvlad@gmail.com>
2019-03-02 18:55:37 +0300

    Add basic rating layout

--- app/src/main/java/com/bendezu/tinkofffintech/courses/RatingFragment.kt
+++ app/src/main/java/com/bendezu/tinkofffintech/courses/RatingFragment.kt
@@ -6,10 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bendezu.tinkofffintech.R
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 class RatingFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_rating, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        detailsButton.setOnClickListener {  }
     }
 }
