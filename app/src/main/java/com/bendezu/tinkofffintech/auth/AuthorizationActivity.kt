252e3672954b07ebd124253fe5ba534f5d363ccc
Bendezu Vladislav <bendezuvlad@gmail.com>
2019-03-23 19:34:00 +0300

    Add empty authorization activity

---
+++ app/src/main/java/com/bendezu/tinkofffintech/auth/AuthorizationActivity.kt
@@ -0,0 +1,13 @@
+package com.bendezu.tinkofffintech.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bendezu.tinkofffintech.R
+
+class AuthorizationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+    }
+}
